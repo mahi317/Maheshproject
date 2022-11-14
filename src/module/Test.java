c135d49a9065a0e0d16a46c8d0545e2e778032e8
Mahesh <gmaheshwar317@gmail.com>
2022-11-14 21:31:18 +0530

    malli changes

--- src/module/Test.java
+++ src/module/Test.java
@@ -6,11 +6,11 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Java project code base");
 		
-		System.out.println("mahesh code base");
+		System.out.println("malli code base - 1");
 
-		System.out.println("mahesh code base1");
+		System.out.println("malli code base - 2 ");
 		
-		System.out.println("malli code base 2");
+		System.out.println("malli code base - 3 ");
 
 
 		
