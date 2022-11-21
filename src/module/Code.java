5113e35ad09408ffc8b6e654c300a323a440ba52
Mahesh <gmaheshwar317@gmail.com>
2022-11-21 12:41:02 +0530

    changes on Malli branch

--- src/module/Code.java
+++ src/module/Code.java
@@ -3,8 +3,8 @@ package module;
 public class Code {
 	
 	public void m1() {
+		System.out.println("Malli Branch");
 		System.out.println("M1 method");
-		System.out.println("M2 method");
 		System.out.println("M2 method");
 
 
