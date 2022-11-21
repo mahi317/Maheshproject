001fc058ffc0436c2c3674b6c6d204ec604d3a19
Mahesh <gmaheshwar317@gmail.com>
2022-11-21 12:38:04 +0530

    changes in mahesh branch

--- src/module/Code.java
+++ src/module/Code.java
@@ -3,10 +3,11 @@ package module;
 public class Code {
 	
 	public void m1() {
+		System.out.println("Mahesh branch");
 		System.out.println("M1 method");
 		System.out.println("M2 method");
 		System.out.println("M2 method");
-
+		
 
 	}
 
